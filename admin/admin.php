8b0f870c44d2c24fc6f80e42988a142f6e53e74f
rugerclaus <rogerrock12games@gmail.com>
2022-11-24 03:01:48 -0700

    Implemented admin object

---
+++ admin/admin.php
@@ -0,0 +1,17 @@
+<?php 
+    
+    include '../backend/Model/objects.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin || <?php echo $admin->name; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
