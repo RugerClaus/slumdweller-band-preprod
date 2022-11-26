5cc449e5e65b1276d423f69a51223aa8f6edccb7
rugerclaus <rogerrock12games@gmail.com>
2022-11-25 19:54:10 -0700

    updated products page

--- frontend/views/albums_and_merch.php
+++ frontend/views/albums_and_merch.php
@@ -4,7 +4,11 @@
     ?>
 
     <div class="product">
-        <img src="../admin/<?php echo $row['image1']; ?>" alt="error">
+        <h2><?php echo $row['name']; ?></h2>
+        <img src="../admin/<?php echo $row['image1']; ?>" alt="<?php echo $row['name']; ?>">
+        <form action="details.php">
+            <input type="submit" name="details" value="View Details" class="btn">
+        </form>
     </div>
 
     <?php } ?>
