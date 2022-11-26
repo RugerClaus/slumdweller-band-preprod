4678ba7de44eb47200478b92e8845132d87c3de9
rugerclaus <rogerrock12games@gmail.com>
2022-11-25 19:34:46 -0700

    file update system works

--- admin/backend/editProduct.php
+++ admin/backend/editProduct.php
@@ -8,12 +8,18 @@
         $instock = $_POST['instock'];
         $desc = $_POST['description'];
         $shopQTY = $_POST['stockquantity'];
-        $uploaddir = '../admin/assets/images/';
+        $uploaddir = '../../admin/assets/images/';
         $uploadfile1 = $uploaddir . basename($_FILES['image1']['name']);
         $uploadfile2 = $uploaddir . basename($_FILES['image2']['name']);
         $uploadfile3 = $uploaddir . basename($_FILES['image3']['name']);
         $uploadfile4 = $uploaddir . basename($_FILES['image4']['name']);
         $uploadfile5 = $uploaddir . basename($_FILES['image5']['name']);
+        $u1 = substr($uploadfile1,3);
+        $u2 = substr($uploadfile2,3);
+        $u3 = substr($uploadfile3,3);
+        $u4 = substr($uploadfile4,3);
+        $u5 = substr($uploadfile5,3);
+        
 
         echo '<pre>';
         if (move_uploaded_file($_FILES['image1']['tmp_name'], $uploadfile1)) {
@@ -44,10 +50,9 @@
         echo 'Here is some more debugging info:';
         print_r($_FILES); 
 
-        $query = "UPDATE `products`  SET `name`='$name',`description`='$desc',`type`='$type',`in_stock`='$instock',`price`='$price',`image1`='$uploadfile1',`image2`='$uploadfile2',`image3`='$uploadfile3',`image4`='$uploadfile4',`image5`='$uploadfile5', `stockqty`='$shopQTY' where `product_id` = '$id'";
+        $query = "UPDATE `products`  SET `name`='$name',`description`='$desc',`type`='$type',`in_stock`='$instock',`price`='$price',`image1`='$u1',`image2`='$u2',`image3`='$u3',`image4`='$u4',`image5`='$u5', `stockqty`='$shopQTY' where `product_id` = '$id'";
         $slumdweller->query($query);
         $_SESSION['page'] = 'edit_product';
         $page = $_SESSION['page'];
-        header("location: ../admin.php?=$page");
     }
 ?>
