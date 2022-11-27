a7a6e0b1e05570dd1064d5aece3227f7e1366e73
rugerclaus <rogerrock12games@gmail.com>
2022-11-26 23:43:16 -0700

    got images to display properly

--- frontend/views/details.php
+++ frontend/views/details.php
@@ -6,10 +6,28 @@
     <p><?php echo $_SESSION['product']['desc'] ?></p>
     <span class="currencySymbol"><?php echo $ipdat->geoplugin_currencySymbol . "\n"; ?><span class="price"><?php echo intval($_SESSION['product']['price']); ?></span></span>
     <div class="subimages">
-        <img src="<?php if(strlen($_SESSION['product']['image2']) > 1){ echo '../admin/' . $_SESSION['product']['image2'];}else{echo "No Image";} ?>" class="subimage">
-        <img src="<?php if(strlen($_SESSION['product']['image3']) > 1){ echo '../admin/' . $_SESSION['product']['image3'];}else{echo "No Image";} ?>" class="subimage">
-        <img src="<?php if(strlen($_SESSION['product']['image4']) > 1){ echo '../admin/' . $_SESSION['product']['image4'];}else{echo "No Image";} ?>" class="subimage">
-        <img src="<?php if(strlen($_SESSION['product']['image5']) > strlen('../admin/assets/images')){ echo '../admin/' . $_SESSION['product']['image5'];}?>" class="subimage">
+    <?php 
+            if(strlen($_SESSION['product']['image2']) < strlen('../admin/assets/images/a')){
+                return;
+            }else {
+                echo "<img src='../admin/" . $_SESSION['product']['image2'] . "' class='subimage'/>";
+            }
+            if(strlen($_SESSION['product']['image3']) < strlen('../admin/assets/images/a')){
+                return;
+            }else {
+                echo "<img src='../admin/" . $_SESSION['product']['image3'] . "' class='subimage'/>";
+            }
+            if(strlen($_SESSION['product']['image4']) < strlen('../admin/assets/images/a')){
+                return;
+            }else {
+                echo "<img src='../admin/" . $_SESSION['product']['image4'] . "' class='subimage'/>";
+            }
+            if(strlen($_SESSION['product']['image5']) < strlen('../admin/assets/images/a')){
+                return;
+            }else {
+                echo "<img src='../admin/" . $_SESSION['product']['image5'] . "' class='subimage'/>";
+            }
+        ?>
     </div>
 
 </section>
